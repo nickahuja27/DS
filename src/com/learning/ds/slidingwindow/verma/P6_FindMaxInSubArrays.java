f2365a93504e836e1e4282732c266a5ec6fc9b1c
nickahujaaol <nick_ahuja@aol.com>
2022-12-12 22:07:41 -0800

    Practice

---
+++ src/com/learning/ds/slidingwindow/verma/P6_FindMaxInSubArrays.java
@@ -0,0 +1,32 @@
+package com.learning.ds.slidingwindow.verma;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P6_FindMaxInSubArrays {
+    public static void main(String[] args) {
+        int[] inArray = {1, 3, -1, -3, 5, 3, 6, 7};
+        findMax(inArray, 3);
+    }
+    
+    private static void findMax(int[] inArray, int windowSize) {
+        int i = 0;
+        int j = 0;
+        Queue<Integer> queue = new LinkedList<>();
+
+        while (j < inArray.length) {
+            while (!queue.isEmpty() && queue.peek() < inArray[j]) {
+                queue.poll();
+            }
+            queue.add(inArray[j]);
+
+            if(j - i + 1 < windowSize) {
+                j++;
+            } else if(j - i + 1 == windowSize) {
+                System.out.println("Max num in current window: " + queue.peek());
+                i++;
+                j++;
+            }
+        }
+    }
+}
